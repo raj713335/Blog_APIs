acfbe9c9b8714bc3d4251cb5bc97c6c5a0c6d9b1
ARNAB DAS <raj713335@gmail.com>
2020-12-25 18:37:05 +0530

    Initial Commit

--- blog_project/posts/views.py
+++ blog_project/posts/views.py
@@ -3,7 +3,7 @@ from rest_framework import generics
 from .models import Post
 from .serializers import PostSerializer
 
-# Create your views here.
+# Create your views here...
 
 
 class PostList(generics.ListCreateAPIView):
