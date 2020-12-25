368585af84a74cf95c369ac171dc798eca5beab8
ARNAB DAS <raj713335@gmail.com>
2020-12-25 23:17:58 +0530

    Viewsets and Routers Complete

--- blog_project/posts/urls.py
+++ blog_project/posts/urls.py
@@ -1,9 +1,11 @@
 # posts/urls.py
 
 from django.urls import path
-from .views import PostList,PostDetail
+from .views import PostList,PostDetail,UserList,UserDetail
 
 urlpatterns=[
+    path('users/',UserList.as_view()),
+    path('users/<int:pk>/',UserDetail.as_view()),
     path('<int:pk>/',PostDetail.as_view()),
     path('',PostList.as_view()),
 ]
--- blog_project/posts/views.py
+++ blog_project/posts/views.py
@@ -1,7 +1,9 @@
 # posts/views.py
+
+from django.contrib.auth import get_user_model
 from rest_framework import generics,permissions
 from .models import Post
-from .serializers import PostSerializer
+from .serializers import PostSerializer,UserSerializer
 from .permissions import IsAuthorOrReadOnly
 
 # Create your views here....
@@ -18,3 +20,12 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+class UserList(generics.ListCreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
