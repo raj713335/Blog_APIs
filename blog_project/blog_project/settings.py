d7a2ff9ceaccaf82f61e2fba050023dc690f3f2b
ARNAB DAS <raj713335@gmail.com>
2020-12-25 18:11:34 +0530

    Initial Commit

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+
+    # 3rd-party apps
+    'rest_framework',
+
     #Local
     'posts.apps.PostsConfig',
 ]
@@ -121,3 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK={
+    'DEFAULT_PERMISSION_CLASSES':[
+        'rest_framework.permissions.AllowAny',
+    ]
+}
